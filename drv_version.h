75073a6448bb283d4d2dfdeb788b0c807b85bfed
up-n-atom <234549+up-n-atom@users.noreply.github.com>
2026-08-19 00:30:02 -0400

    Squashed 'intel/ltq-voice-kpi2udp/' content from commit 6efd78d177d6
    git-subtree-dir: intel/ltq-voice-kpi2udp
    git-subtree-split: 6efd78d177d60a55592cadd4af4ce9f5b3925b0e

---
+++ drv_version.h
@@ -0,0 +1,22 @@
+#ifndef _DRV_VERSION_H
+#define _DRV_VERSION_H
+
+/******************************************************************************
+
+  Copyright 2008-2009 Infineon Technologies AG
+  Copyright 2009-2014 Lantiq Deutschland GmbH
+  Copyright 2017, Intel Corporation.
+
+  For licensing information, see the file 'LICENSE' in the root folder of
+  this software module.
+
+******************************************************************************/
+
+
+#define MAJORSTEP    3
+#define MINORSTEP    1
+#define VERSIONSTEP  0
+#define VERS_TYPE    0
+
+#endif /* _DRV_VERSION_H */
+
