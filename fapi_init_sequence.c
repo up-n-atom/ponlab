c338973f9b2c0dac972ac7260ea1906f2fbb568b
up-n-atom <234549+up-n-atom@users.noreply.github.com>
2026-08-19 00:26:00 -0400

    Squashed 'maxlinear/libhelper/' content from commit d4e03ed358
    git-subtree-dir: maxlinear/libhelper
    git-subtree-split: d4e03ed35805462e4fffbf6737df4beffdcd0752

---
+++ fapi_init_sequence.c
@@ -0,0 +1,28 @@
+/*******************************************************************************
+
+  Copyright © 2020 MaxLinear, Inc.
+
+  For licensing information, see the file 'LICENSE' in the root folder of
+  this software module.
+
+*******************************************************************************/
+
+#include "fapi_init_sequence.h"
+#include <syslog.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int sequence = 0;
+
+void log_fapi_init_msg(const char* file, const char *func, const int line)
+{
+	syslog(LOG_DEBUG, "FAPI_INIT: %s [%s:%d] FAPI sequence number: %d Process ID: %d\n", file, func, line, ++sequence, getpid());
+	
+}
+
+void log_fapi_done_msg(const char* file, const char *func, const int line)
+{
+		
+	syslog(LOG_DEBUG, "FAPI_INIT: %s [%s:%d]\n", file, func, line);
+}
